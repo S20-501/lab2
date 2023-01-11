4071bbdd7e86d7d77fe0071e903441c93ff97bbb
Rachu <70590717+RachUwU@users.noreply.github.com>
2023-01-12 01:18:48 +0300

    Update GSMRegistr.vhd

--- FunctionalSignalGenerator/Protocol Analyzer/GSMRegistr.vhd
+++ FunctionalSignalGenerator/Protocol Analyzer/GSMRegistr.vhd
@@ -9,7 +9,8 @@ entity GSMRegister is
 		
 		--Wishbone
 		WB_Addr		: in	std_logic_vector( 15 downto 0 );
-		WB_DataOut	: out	std_logic_vector( 15 downto 0 );
+		WB_DataOut_0	: out	std_logic_vector( 15 downto 0 );
+		WB_DataOut_2	: out	std_logic_vector( 15 downto 0 );
 		WB_DataIn	: in	std_logic_vector( 15 downto 0 );
 		WB_WE			: in 	std_logic;
 		WB_Sel		: in 	std_logic_vector( 1 downto 0 );
@@ -19,12 +20,12 @@ entity GSMRegister is
 		WB_Ack		: out std_logic;
 		WB_CTI		: in	std_logic_vector(2 downto 0);
 
-		PRT_O						: out 	std_logic_vector( 15 downto 0 ); --Ð´Ð°Ð½Ð½ÑÐµ Ð´Ð»Ñ ÐºÐ¾Ð´Ð¸ÑÐ¾Ð²Ð°Ð½Ð¸Ñ Ð¸ Ð¼Ð¾Ð´ÑÐ»ÑÑÐ¸Ð¸
+		PRT_O						: out 	std_logic_vector( 15 downto 0 ); --данные для кодирования и модуляции
 --		Amplitude_OUT			: out 	std_logic_vector( 15 downto 0);
 --		StartPhase_OUT			: out 	std_logic_vector( 15 downto 0);
 		CarrierFrequency_OUT	: out 	std_logic_vector(31 downto 0);
 		SymbolFrequency_OUT	: out 	std_logic_vector( 31 downto 0);
-		DataPort_OUT			: out 	std_logic_vector( 15 downto 0);--Ð¸Ð´ÐµÑ Ð² FIFO
+		DataPort_OUT			: out 	std_logic_vector( 15 downto 0);--идет в FIFO
 		wrreq						: out 	std_logic;
 		full : in std_logic
 	);
@@ -34,10 +35,11 @@ architecture Behavior of GSMRegister is
 	signal QL_r: std_logic_vector( 7 downto 0 );
 --	signal Amplitude_r: std_logic_vector( 15 downto 0 );
 --	signal Start_Phase_r: std_logic_vector( 15 downto 0 );
-	signal WB_DataOut_r: std_logic_vector(15 downto 0);
+	signal WB_DataOut_0_r: std_logic_vector(15 downto 0);
+	signal WB_DataOut_2_r: std_logic_vector(15 downto 0);
 	signal Carrier_Frequency_r: std_logic_vector( 31 downto 0 );
 	signal Symbol_Frequency_r: std_logic_vector( 31 downto 0 );
-	signal DataPort_r: std_logic_vector( 15 downto 0 ); -- Ð¿Ð¾Ð¹Ð´ÐµÑ Ð² Ð¤ÐÐ¤Ð
+	signal DataPort_r: std_logic_vector( 15 downto 0 ); -- пойдет в ФИФО
 	signal Ack_r: std_logic;
 	signal wrreq_r: std_logic;
 begin
@@ -54,12 +56,13 @@ begin
 				DataPort_r <= x"0000";
 				wrreq_r <= '0';
 				Ack_r <= '0';
-				WB_DataOut_r <= "0000000000000000";
+				WB_DataOut_0_r <= "0000000000000000";
+				WB_DataOut_2_r <= "0000000000000000";
 			elsif (rising_edge(clk)) then
 			
 				if ((WB_STB and WB_Cyc_2) = '1') then
 					if(Ack_r = '0') then
-						if (WB_Addr = x"000C")then
+						if (WB_Addr = x"020C")then
 						   if (full = '0') then
 						     Ack_r <= '1';
 						   end if;
@@ -80,7 +83,7 @@ begin
 				end if;
 				--
 				
-				if (WB_Cyc_2 = '1' and WB_WE = '1' and WB_STB = '1' and WB_Addr = x"000C") then
+				if (WB_Cyc_2 = '1' and WB_WE = '1' and WB_STB = '1' and WB_Addr = x"020C") then
 					if (full = '0') then
 						wrreq_r <= '1';
 					else
@@ -102,7 +105,7 @@ begin
 						end if;
 					elsif(WB_WE = '0' and WB_STB = '1') then
 						if(WB_Addr = x"0000") then
-							WB_DataOut_r( 15 downto 0 ) <= QH_r & QL_r;
+							WB_DataOut_0_r( 15 downto 0 ) <= QH_r & QL_r;
 						end if;
 					end if;
 					--
@@ -123,15 +126,15 @@ begin
 						end if;
 					elsif(WB_WE = '0' and WB_STB = '1') then
 						if(WB_Addr = x"0004") then
-							WB_DataOut_r <= Carrier_Frequency_r( 31 downto 16 );
+							WB_DataOut_2_r <= Carrier_Frequency_r( 31 downto 16 );
 						elsif(WB_Addr = x"0006") then
-							WB_DataOut_r <= Carrier_Frequency_r( 15 downto 0 );
+							WB_DataOut_2_r <= Carrier_Frequency_r( 15 downto 0 );
 						elsif(WB_Addr = x"0008") then
-							WB_DataOut_r <= Symbol_Frequency_r( 31 downto 16 );
+							WB_DataOut_2_r <= Symbol_Frequency_r( 31 downto 16 );
 						elsif(WB_Addr = x"000A") then
-							WB_DataOut_r <= Symbol_Frequency_r( 15 downto 0 );
+							WB_DataOut_2_r <= Symbol_Frequency_r( 15 downto 0 );
 						elsif(WB_Addr = x"000C") then
-							WB_DataOut_r <= DataPort_r;
+							WB_DataOut_2_r <= DataPort_r;
 						end if;
 					end if;
 				end if;
@@ -145,5 +148,6 @@ begin
 	DataPort_OUT <= DataPort_r;
 	wrreq <= wrreq_r;
 	WB_Ack <= Ack_r;
-	WB_DataOut <= WB_DataOut_r;
+	WB_DataOut_0 <= WB_DataOut_0_r;
+	WB_DataOut_2 <= WB_DataOut_2_r;
 end architecture Behavior;
