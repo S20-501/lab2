04dee993d05371d4078ba7603bec8aa82ce4ee71
Rachu <70590717+RachUwU@users.noreply.github.com>
2023-01-22 00:20:11 +0300

    Update fifoOld.vhd

--- FunctionalSignalGenerator/Protocol Analyzer/fifoOld.vhd
+++ FunctionalSignalGenerator/Protocol Analyzer/fifoOld.vhd
@@ -46,8 +46,8 @@ ENTITY GSMRegistr_FIFO IS
 		data		: IN STD_LOGIC_VECTOR (15 DOWNTO 0);
 		rdreq		: IN STD_LOGIC ;
 		wrreq		: IN STD_LOGIC ;
+		almost_full		: OUT STD_LOGIC ;
 		empty		: OUT STD_LOGIC ;
-		full		: OUT STD_LOGIC ;
 		q		: OUT STD_LOGIC_VECTOR (15 DOWNTO 0);
 		usedw		: OUT STD_LOGIC_VECTOR (9 DOWNTO 0)
 	);
@@ -66,6 +66,7 @@ ARCHITECTURE SYN OF gsmregistr_fifo IS
 	COMPONENT scfifo
 	GENERIC (
 		add_ram_output_register		: STRING;
+		almost_full_value		: NATURAL;
 		intended_device_family		: STRING;
 		lpm_numwords		: NATURAL;
 		lpm_showahead		: STRING;
@@ -81,22 +82,23 @@ ARCHITECTURE SYN OF gsmregistr_fifo IS
 			data	: IN STD_LOGIC_VECTOR (15 DOWNTO 0);
 			rdreq	: IN STD_LOGIC ;
 			wrreq	: IN STD_LOGIC ;
+			almost_full	: OUT STD_LOGIC ;
 			empty	: OUT STD_LOGIC ;
-			full	: OUT STD_LOGIC ;
 			q	: OUT STD_LOGIC_VECTOR (15 DOWNTO 0);
 			usedw	: OUT STD_LOGIC_VECTOR (9 DOWNTO 0)
 	);
 	END COMPONENT;
 
 BEGIN
-	empty    <= sub_wire0;
-	full    <= sub_wire1;
+	almost_full    <= sub_wire0;
+	empty    <= sub_wire1;
 	q    <= sub_wire2(15 DOWNTO 0);
 	usedw    <= sub_wire3(9 DOWNTO 0);
 
 	scfifo_component : scfifo
 	GENERIC MAP (
 		add_ram_output_register => "OFF",
+		almost_full_value => 1023,
 		intended_device_family => "Cyclone 10 LP",
 		lpm_numwords => 1024,
 		lpm_showahead => "OFF",
@@ -112,8 +114,8 @@ BEGIN
 		data => data,
 		rdreq => rdreq,
 		wrreq => wrreq,
-		empty => sub_wire0,
-		full => sub_wire1,
+		almost_full => sub_wire0,
+		empty => sub_wire1,
 		q => sub_wire2,
 		usedw => sub_wire3
 	);
@@ -127,13 +129,13 @@ END SYN;
 -- ============================================================
 -- Retrieval info: PRIVATE: AlmostEmpty NUMERIC "0"
 -- Retrieval info: PRIVATE: AlmostEmptyThr NUMERIC "-1"
--- Retrieval info: PRIVATE: AlmostFull NUMERIC "0"
--- Retrieval info: PRIVATE: AlmostFullThr NUMERIC "-1"
+-- Retrieval info: PRIVATE: AlmostFull NUMERIC "1"
+-- Retrieval info: PRIVATE: AlmostFullThr NUMERIC "1023"
 -- Retrieval info: PRIVATE: CLOCKS_ARE_SYNCHRONIZED NUMERIC "0"
 -- Retrieval info: PRIVATE: Clock NUMERIC "0"
 -- Retrieval info: PRIVATE: Depth NUMERIC "1024"
 -- Retrieval info: PRIVATE: Empty NUMERIC "1"
--- Retrieval info: PRIVATE: Full NUMERIC "1"
+-- Retrieval info: PRIVATE: Full NUMERIC "0"
 -- Retrieval info: PRIVATE: INTENDED_DEVICE_FAMILY STRING "Cyclone 10 LP"
 -- Retrieval info: PRIVATE: LE_BasedFIFO NUMERIC "0"
 -- Retrieval info: PRIVATE: LegacyRREQ NUMERIC "1"
@@ -159,6 +161,7 @@ END SYN;
 -- Retrieval info: PRIVATE: wsUsedW NUMERIC "0"
 -- Retrieval info: LIBRARY: altera_mf altera_mf.altera_mf_components.all
 -- Retrieval info: CONSTANT: ADD_RAM_OUTPUT_REGISTER STRING "OFF"
+-- Retrieval info: CONSTANT: ALMOST_FULL_VALUE NUMERIC "1023"
 -- Retrieval info: CONSTANT: INTENDED_DEVICE_FAMILY STRING "Cyclone 10 LP"
 -- Retrieval info: CONSTANT: LPM_NUMWORDS NUMERIC "1024"
 -- Retrieval info: CONSTANT: LPM_SHOWAHEAD STRING "OFF"
@@ -168,10 +171,10 @@ END SYN;
 -- Retrieval info: CONSTANT: OVERFLOW_CHECKING STRING "ON"
 -- Retrieval info: CONSTANT: UNDERFLOW_CHECKING STRING "ON"
 -- Retrieval info: CONSTANT: USE_EAB STRING "ON"
+-- Retrieval info: USED_PORT: almost_full 0 0 0 0 OUTPUT NODEFVAL "almost_full"
 -- Retrieval info: USED_PORT: clock 0 0 0 0 INPUT NODEFVAL "clock"
 -- Retrieval info: USED_PORT: data 0 0 16 0 INPUT NODEFVAL "data[15..0]"
 -- Retrieval info: USED_PORT: empty 0 0 0 0 OUTPUT NODEFVAL "empty"
--- Retrieval info: USED_PORT: full 0 0 0 0 OUTPUT NODEFVAL "full"
 -- Retrieval info: USED_PORT: q 0 0 16 0 OUTPUT NODEFVAL "q[15..0]"
 -- Retrieval info: USED_PORT: rdreq 0 0 0 0 INPUT NODEFVAL "rdreq"
 -- Retrieval info: USED_PORT: usedw 0 0 10 0 OUTPUT NODEFVAL "usedw[9..0]"
@@ -180,8 +183,8 @@ END SYN;
 -- Retrieval info: CONNECT: @data 0 0 16 0 data 0 0 16 0
 -- Retrieval info: CONNECT: @rdreq 0 0 0 0 rdreq 0 0 0 0
 -- Retrieval info: CONNECT: @wrreq 0 0 0 0 wrreq 0 0 0 0
+-- Retrieval info: CONNECT: almost_full 0 0 0 0 @almost_full 0 0 0 0
 -- Retrieval info: CONNECT: empty 0 0 0 0 @empty 0 0 0 0
--- Retrieval info: CONNECT: full 0 0 0 0 @full 0 0 0 0
 -- Retrieval info: CONNECT: q 0 0 16 0 @q 0 0 16 0
 -- Retrieval info: CONNECT: usedw 0 0 10 0 @usedw 0 0 10 0
 -- Retrieval info: GEN_FILE: TYPE_NORMAL fifoOld.vhd TRUE
