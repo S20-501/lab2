615e20c61f4676bcd9d84a09df1aa2fd47ecb6b5
Rachu <70590717+RachUwU@users.noreply.github.com>
2023-01-23 14:27:32 +0300

    Update GSMRegistr_top.vhd

--- FunctionalSignalGenerator/Protocol Analyzer/GSMRegistr_top.vhd
+++ FunctionalSignalGenerator/Protocol Analyzer/GSMRegistr_top.vhd
@@ -45,9 +45,9 @@ architecture rtl of GSMRegistr_top is
         rdreq : in STD_LOGIC;
         wrreq : in STD_LOGIC;
         empty : out STD_LOGIC;
-        almost_full : out STD_LOGIC;
+        full : out STD_LOGIC;
         q : out STD_LOGIC_VECTOR (15 DOWNTO 0);
-		  aclr		: in STD_LOGIC 
+		  sclr		: in STD_LOGIC 
  --       usedw : out STD_LOGIC_VECTOR (9 DOWNTO 0)
       );
     end component;
@@ -81,65 +81,14 @@ architecture rtl of GSMRegistr_top is
 		SymbolFrequency_OUT	: out 	std_logic_vector( 31 downto 0);
 		DataPort_OUT			: out 	std_logic_vector( 15 downto 0);--идет в FIFO
 		wrreq						: out 	std_logic;
-		full_fifo 						: in 		std_logic;
-		
-		rdreq_buff 				: out		std_logic;
-		empty_buff 				: in 		std_logic
+		full_fifo 						: in 		std_logic
 	);
 	end component;
-	
-	component ring_buffer
-		generic (
-			RAM_WIDTH : natural;
-			RAM_DEPTH : natural
-		);
-		port (
-			clk : in std_logic;
-			nRst : in std_logic;
-
-			-- Write port
-			wr_en : in std_logic;
-			wr_data : in std_logic_vector(RAM_WIDTH - 1 downto 0);
-
-			-- Read port
-			rd_en : in std_logic;
-			rd_valid : out std_logic;
-			rd_data : out std_logic_vector(RAM_WIDTH - 1 downto 0);
-	
-			-- Flags
-			empty : out std_logic;
-			empty_next : out std_logic;
-			full : out std_logic;
-			full_next : out std_logic;
-
-			-- The number of elements in the FIFO
-			fill_count : out integer range RAM_DEPTH - 1 downto 0
-		);
-	end component;
-
 
 	signal wrreq 		: std_logic 	:= '0';
 	signal full_fifo_r 		: std_logic 	:= '0';
 	signal DataPort_r	: std_logic_vector( 15 downto 0 ) := (others=>'0');
-	signal rdreq_buff : std_logic := '0';
-	signal empty_buff : std_logic := '0';
 	
-	signal WB_DataOut_1_r	: std_logic_vector( 15 downto 0 ) := (others=>'0');
-	signal WB_DataOut_2_r	: std_logic_vector( 15 downto 0 ) := (others=>'0');
-	
-	constant RAM_WIDTH : natural := 16;
-	constant RAM_DEPTH : natural := 1024;
-
-  -- DUT signals
-	
-	signal rd_en : std_logic := '0';
-	signal rd_valid : std_logic;
-	
---	signal empty_buff : std_logic;
-	signal empty_next : std_logic;
-	signal full : std_logic;
-	signal full_next : std_logic;
-	signal fill_count : integer range RAM_DEPTH - 1 downto 0;
 	
 begin
 --		full <= full_r;
@@ -149,7 +98,7 @@ begin
         nRst => nRst,
         WB_Addr => WB_Addr,
         WB_DataOut_0 => WB_DataOut_0,
-		  WB_DataOut_2 => WB_DataOut_1_r,
+		  WB_DataOut_2 => WB_DataOut_2,
         WB_DataIn => WB_DataIn,
         WB_WE => WB_WE,
         WB_Sel => WB_Sel,
@@ -169,9 +118,7 @@ begin
         SymbolFrequency_OUT => SymbolFrequency_OUT,
         DataPort_OUT => DataPort_r,
         wrreq => wrreq,
-		  full_fifo => full_fifo_r,
-		  rdreq_buff => rdreq_buff,
-		  empty_buff => empty_buff
+		  full_fifo => full_fifo_r
 	 );
 						
 
@@ -182,47 +129,10 @@ begin
             rdreq => rdreq,
             wrreq => wrreq,
             empty => empty,
-            almost_full => full_fifo_r,
-            aclr => "not"(nRst),
-				q => q
+				full => full_fifo_r,
+            sclr => "not"(nRst),
+			q => q
 --            usedw => usedw
         );
-	 DUT : ring_buffer
-		 generic map (
-			RAM_WIDTH => RAM_WIDTH,
-			RAM_DEPTH => RAM_DEPTH
-		 )
-		port map (
-			clk => clk,
-			nRst => nRst,
-			
-			wr_en => wrreq,
-			wr_data => DataPort_r,
-			
-			rd_en => rdreq_buff,
-			rd_valid => rd_valid,
-			rd_data => WB_DataOut_2_r,
-			
-			empty => empty_buff,
-			empty_next => empty_next,
-
-			full => full,
-			full_next => full_next,
-			fill_count => fill_count
-		);
-	process(clk, rdreq_buff)
-		begin
-			if( nRst = '0') then
-				WB_DataOut_2 <= (others =>'0');
-				
-			elsif (rising_edge(clk)) then
-				if(WB_Cyc_2 = '1' and WB_WE = '0' and WB_STB = '1' ) then
-					if(WB_Addr = x"000C") then
-						WB_DataOut_2 <= WB_DataOut_2_r;
-					else
-						WB_DataOut_2 <= WB_DataOut_1_r;
-					end if;
-				end if;
-			end if;
-	end process;
+	 
 end architecture;
